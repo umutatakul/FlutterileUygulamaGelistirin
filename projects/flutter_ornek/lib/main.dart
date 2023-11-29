5dd000dcf2d4b0a3d298e065a5d6c81e0654b453
Umut Atakul <atakulumut@gmail.com>
2023-11-29 16:37:57 +0300

    2.3 Değişkenler ve Fonksiyonlar

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:ui' as ui show Canvas, Paint, Path;
 
-
 void main() {
-  print("main çalıştı");
+  print('a'.isNotEmpty);
+  print(1.isEven);
+  var i = 1;
+  print(i);
+  String mainTekCal;
+  mainTekCal = "main" + "tekrar" + "çalıştı ";
 
-  var max = 10;
-  loop(max);
-  print(max);
+  print(mainTekCal);
+
+  int ii = findAr(mainTekCal);
+  print(ii);
+
+  print("main bitmek üzere");
 }
 
-void loop(int max) {
-  for(int i = 1; i <max; ++i){
-    print("number $i");
-  };
+int findAr(String s) {
+  var index = s.indexOf("ar");
+  return index;
 }
-
