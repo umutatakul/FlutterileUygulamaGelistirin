97f773424138682c1f6ed3a1576120d8053088c3
Umut Atakul <atakulumut@gmail.com>
2023-11-30 19:26:12 +0300

    3.1 Değişkenler ve Veri Türleri Birinci kısım

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:ui' as ui show Canvas, Paint, Path;
 
-typedef KonuVeEylemFunc = String Function({String eylem, required String konu});
-
+final globalA = "global değişken a";
 void main() {
- print("main çalıştı");
- var konu = "flutter";
- var eylem = "öğreniyorum";
+  print(globalA);
+  final a = "a";
+  final maindekiA
+  = "a";
+  final d = "değişken" + maindekiA;
+  print("main çalıştı");
+  altProgram();
+  if (1==1) {
 
-KonuVeEylemFunc f = ({required String konu, String eylem = ""}) => konu+' '+eylem;
-
- String hepsi = f(konu: konu, eylem: eylem);
- print(hepsi);
-
-
+    final a = "içerdeki a";
+    print(a);
+  }
 }
 
-void dondur() => 12;
-void dondur(int sayi) => 12;
+void altProgram() {
+  print(globalA);
+  print("alt program çalıştı.");
+  final a = "başka bir a";
+  print(a);
+}
 
-
+class C{
+  int a = 1;
+}
