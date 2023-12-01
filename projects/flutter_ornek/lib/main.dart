f11c06db9425344b88f906d13a445d5481340525
Umut Atakul <atakulumut@gmail.com>
2023-12-01 20:20:38 +0300

    3.8 Flutter Lints ile Temiz Kod  Bölüm 1  Linter : Kodu Temizleme

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -3,40 +3,9 @@ import 'package:flutter/widgets.dart';
 import 'dart:ui' as ui show Canvas, Paint, Path;
 
 void main() {
-  print("merhaba");
-  returnEdecek();
+ String? s;
+ print(s);
 
-}
+ print("merhaba!");
 
-void returnEdecek() {
-  try {
-    print(">main");
-
-    hataliKodCagiran();
-    print("<main");
-  } on FormatException catch (e) {
-    print("format excepiton oldu");
-    rethrow;
-  } catch (e) {
-    print("başka bir  hata oldu");
-    print(e);
-  }finally{
-    print("FINALLY");
-  }
-  print("FINALLY\'den sonra");
-}
-
-void hataliKodCagiran() {
-  print(">hatali kod çağıran");
-  hataliKod();
-  print("<hatali kod çağıran");
-}
-
-void hataliKod() {
-  print(">hatalı kod");
-  throw new Exception("HATA VAR");
-  double.parse("bu bir double değer değildir");
-  String? s = null;
-  print(s!.length);
-  print("<hatali kod");
 }
