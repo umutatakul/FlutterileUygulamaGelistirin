2a2770c25eaeefa3bb205ba7c73b13b68f22f4ea
Umut Atakul <atakulumut@gmail.com>
2023-12-01 15:40:26 +0300

    3.5 Class 2. Kısım :Map ve Class
    Map : İçerik kesin değil, türler yok
    
    class : içerik değişkenlerle temsil ediliyor, türler var

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -4,33 +4,13 @@ import 'dart:ui' as ui show Canvas, Paint, Path;
 
 void main() {
 
-  var liste1 = [1,2,3,];
-  var liste2 = [4,5,6,];
+  final ogrenci = {
+    "adı" : "Ali",
+    "Soyadı" : " Yılmaz",
+    "Yaş" : 18,
+    "cinsiyet" : "erkek",
+    "notu" : 100,
+  };
 
-  print("liste1 : $liste1");
-  print("liste2 : $liste2");
-
-  liste1 = liste2;
-
-  print("liste1 : $liste1");
-  print("liste2 : $liste2");
-
-  liste1[1] = 10;
-  print("10 yaptıktan sonra");
-
-  print("liste1 : $liste1");
-  print("liste2 : $liste2");
-
-  int a = 5;
-  int b = 5;
-
-  String s = "merhaba";
-  final s2 = s.replaceFirst("er", "to");
-  print(s);
-  print(s2);
-
-  /*int, double, String gibi, boolean gibi temel veri türleri değişmez, yani immutable. Fakat List, Set ve Map gibi türler değişir, yani mutable nesnelerdir.
-
-   */
-
+  print(ogrenci);
 }
