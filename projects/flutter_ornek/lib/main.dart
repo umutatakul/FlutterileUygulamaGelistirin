eb4f327cb0096d27a49f9e4d0fca8605b5db6342
Umut Atakul <atakulumut@gmail.com>
2023-12-01 18:55:09 +0300

    3.7 Hata Yakalama  İkinci kısım : Hatanın Yükselmesi

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -3,18 +3,28 @@ import 'package:flutter/widgets.dart';
 import 'dart:ui' as ui show Canvas, Paint, Path;
 
 void main() {
- print("merhaba");
- try{
-    double.parse("bu bir double değer değildir");
-    String? s = null;
-    print(s!.length);
-  }on FormatException catch(e){
-   print("format excepiton oldu");
- }catch(e){
-   print("başka bir  hata oldu");
- }
+  print("merhaba");
+  try {
+    print(">main");
+    hataliKodCagiran();
+    print("<main");
+  } on FormatException catch (e) {
+    print("format excepiton oldu");
+  } catch (e) {
+    print("başka bir  hata oldu");
+  }
 }
 
+void hataliKodCagiran() {
+  print(">hatali kod çağıran");
+  hataliKod();
+  print("<hatali kod çağıran");
+}
 
-
-
+void hataliKod() {
+  print(">hatalı kod");
+  double.parse("bu bir double değer değildir");
+  String? s = null;
+  print(s!.length);
+  print("<hatali kod");
+}
