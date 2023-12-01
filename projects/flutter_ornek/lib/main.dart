6d8bbd43225985a9889067cd296059463bc5781a
Umut Atakul <atakulumut@gmail.com>
2023-12-01 20:35:44 +0300

    3.8 Flutter Lints ile Temiz Kod  Bölüm 2  Linter Ayarları

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -1,11 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'dart:ui' as ui show Canvas, Paint, Path;
-
 void main() {
- String? s;
- print(s);
 
  print("merhaba!");
-
 }
