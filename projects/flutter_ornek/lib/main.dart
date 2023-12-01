977b92a9cb255bd8acb651d99091982614c3775a
Umut Atakul <atakulumut@gmail.com>
2023-12-01 21:30:44 +0300

    3.9 Kütüphane Kullanma Birinci bölüm : package bulma ve kullanma

--- projects/flutter_ornek/lib/main.dart
+++ projects/flutter_ornek/lib/main.dart
@@ -1,5 +1,12 @@
+import 'package:english_words/english_words.dart';
+import 'package:uuid/uuid.dart';
+
 void main() {
- const i = 5;
- print(i);
- print("merhaba!");
+  print("merhaba!");
+
+  print(generateWordPairs().first);
+
+  var uuid = Uuid();
+  var v1 = uuid.v1();
+  print(v1);
 }
